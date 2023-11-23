906306373d28a6e0b2b65b383f2f415343a90f2b
dddingnan <silenceapple1027@gmail.com>
2023-11-22 23:52:08 -0500

    feat: refactor the user interface

--- src/main/java/edu/bu/met/cs665/user/UserInterface.java
+++ src/main/java/edu/bu/met/cs665/user/UserInterface.java
@@ -1,5 +1,7 @@
 package edu.bu.met.cs665.user;
 
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 import java.sql.SQLException;
 import java.util.List;
 import java.util.Map;
@@ -30,97 +32,87 @@ public class UserInterface {
 
     public void start() throws InvalidDataException, Exception {
         while (true) {
-            System.out.println("Please select your current location:");
-            for (int i = 0; i < locations.size(); i++) {
-                System.out.println((i + 1) + ". " + locations.get(i).getName());
-            }
+            Location currentLocation = selectCurrentLocation();
+            Airplane selectedAirplane = selectAirplane();
+            displayReachableLocations(currentLocation, selectedAirplane);
 
-            int locationIndex = -1;
-            while (locationIndex < 0 || locationIndex >= locations.size()) {
-                System.out.print("Enter a number between 1 and " + locations.size() + ": ");
-                try {
-                    String input = scanner.next();
-                    if (!input.matches("\\d+")) { // Check if input not an integer number
-                        throw new InvalidDataException("Invalid data input. Please enter an integer number.",
-                                "UserInterface - Location - Not integer", input);
-                    }
-
-                    locationIndex = Integer.parseInt(input) - 1;
-
-                    if (locationIndex < 0 || locationIndex >= locations.size()) { // Check if input is within the valid
-                        throw new InvalidDataException(
-                                "Invalid input range. Please enter a number between 1 and " + locations.size(),
-                                "UserInterface - Not within the valid range", input);
-                    }
-
-                } catch (InvalidDataException e) {
-                    System.out.println(e.getMessage());
-                    scanner.nextLine(); // discard the invalid input
-                    locationIndex = -1; // Reset the index, forcing the loop to repeat
-                    continue;
-                }
-            }
-            Location currentLocation = locations.get(locationIndex);
-
-            System.out.println("Please select your airplane type:");
-            for (int i = 0; i < airplanes.size(); i++) {
-                System.out.println((i + 1) + ". " + airplanes.get(i).getName());
-            }
-
-            int airplaneIndex = -1;
-            while (airplaneIndex < 0 || airplaneIndex >= airplanes.size()) {
-                System.out.print("Enter a number between 1 and " + airplanes.size() + ": ");
-                try {
-                    String input = scanner.next();
-                    if (!input.matches("\\d+")) { // Check if input not an integer number
-                        throw new InvalidDataException("Invalid data input. Please enter an integer number.",
-                                "UserInterface - Airplane - Not integer", input);
-                    }
-
-                    airplaneIndex = Integer.parseInt(input) - 1;
-
-                    if (airplaneIndex < 0 || airplaneIndex >= locations.size()) { // Check if input is within the valid
-                        // range
-                        throw new InvalidDataException(
-                                "Invalid input range. Please enter a number between 1 and " + airplanes.size(),
-                                "UserInterface - Not within the valid range", input);
-                    }
-
-                } catch (InvalidDataException e) {
-                    System.out.println(e.getMessage());
-                    scanner.nextLine(); // discard the invalid input
-                    airplaneIndex = -1; // Reset the index, forcing the loop to repeat
-                    continue;
-                }
-            }
-            Airplane airplane = airplanes.get(airplaneIndex);
-
-            Map<Location, FlightData> reachableLocations = travelCalculator.calculateReachableLocations(airplane,
-                    currentLocation);
-            System.out.println("Potential city destinations on a single tank of fuel:");
-            int index = 1;
-            for (Map.Entry<Location, FlightData> entry : reachableLocations.entrySet()) {
-                Location location = entry.getKey();
-                FlightData flightData = entry.getValue();
-                double duration = flightData.getDuration();
-                double fuelConsumption = flightData.getFuelConsumption();
-                double CO2Emissions = flightData.getFutureCO2Emissions(); // Here we retrieve the result
-                double flightCost = flightData.getFutureFlightCost(); // Here we retrieve the result
-
-                System.out.println("\n" + index++ + ". Destination: " + location.getName());
-                System.out.println("   Estimated flight duration: " + String.format("%.2f", duration) + " hours");
-                System.out.println("   Fuel consumption: " + String.format("%.2f", fuelConsumption) + " gallons");
-                System.out.println("   CO2 Emissions: " + String.format("%.2f", CO2Emissions) + " kg");
-                System.out.println("   Estimated Flight Cost: $" + String.format("%.2f", flightCost));
-            }
-
-            System.out.println("--------------------------------------------------------");
             if (!promptForContinue()) {
+                System.out.println("--------------------------------------------------------");
                 System.out.println("See you next time~!");
                 break;
             }
         }
         scanner.close();
+    }
+
+    private Location selectCurrentLocation() throws InvalidDataException {
+        System.out.println("Please select your current location:");
+        displayOptions(locations, "getName");
+
+        int locationIndex = getValidUserInput(locations.size());
+        return locations.get(locationIndex);
+    }
+
+    private Airplane selectAirplane() throws InvalidDataException {
+        System.out.println("Please select your airplane type:");
+        displayOptions(airplanes, "getName");
+
+        int airplaneIndex = getValidUserInput(airplanes.size());
+        return airplanes.get(airplaneIndex);
+    }
+
+    private void displayReachableLocations(Location currentLocation, Airplane airplane) throws Exception {
+        Map<Location, FlightData> reachableLocations = travelCalculator.calculateReachableLocations(airplane,
+                currentLocation);
+        System.out.println("Potential city destinations on a single tank of fuel:");
+        int index = 1;
+        for (Map.Entry<Location, FlightData> entry : reachableLocations.entrySet()) {
+            printFlightData(index++, entry.getKey(), entry.getValue());
+        }
+    }
+
+    private void printFlightData(int index, Location location, FlightData flightData) {
+        System.out.println("\n" + index + ". Destination: " + location.getName());
+        System.out
+                .println("   Estimated flight duration: " + String.format("%.2f", flightData.getDuration()) + " hours");
+        System.out
+                .println("   Fuel consumption: " + String.format("%.2f", flightData.getFuelConsumption()) + " gallons");
+        System.out.println("   CO2 Emissions: " + String.format("%.2f", flightData.getFutureCO2Emissions()) + " kg");
+        System.out.println("   Estimated Flight Cost: $" + String.format("%.2f", flightData.getFutureFlightCost()));
+    }
+
+    private int getValidUserInput(int size) throws InvalidDataException {
+        int index = -1;
+        while (index < 0 || index >= size) {
+            System.out.print("Enter a number between 1 and " + size + ": ");
+            index = validateUserInput(scanner.next(), size) - 1;
+        }
+        return index;
+    }
+
+    private int validateUserInput(String input, int size) throws InvalidDataException {
+        if (!input.matches("\\d+")) {
+            throw new InvalidDataException("Invalid data input. Please enter an integer number.",
+                    "UserInterface - Not integer", input);
+        }
+        int parsedInput = Integer.parseInt(input);
+        if (parsedInput < 1 || parsedInput > size) {
+            throw new InvalidDataException("Invalid input range. Please enter a number between 1 and " + size,
+                    "UserInterface - Out of range", input);
+        }
+        return parsedInput;
+    }
+
+    private <T> void displayOptions(List<T> options, String methodName) {
+        for (int i = 0; i < options.size(); i++) {
+            try {
+                Method method = options.get(i).getClass().getMethod(methodName);
+                String name = (String) method.invoke(options.get(i));
+                System.out.println((i + 1) + ". " + name);
+            } catch (NoSuchMethodException | IllegalAccessException | InvocationTargetException e) {
+                e.printStackTrace();
+            }
+        }
     }
 
     private boolean promptForContinue() {
