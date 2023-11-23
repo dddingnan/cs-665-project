c79e436cf064e7befb422b1ae5b2c2055b85a629
dddingnan <silenceapple1027@gmail.com>
2023-11-22 22:32:47 -0500

    feat: add select airplane data

--- src/main/java/edu/bu/met/cs665/database/repository/AirplaneRepository.java
+++ src/main/java/edu/bu/met/cs665/database/repository/AirplaneRepository.java
@@ -2,13 +2,22 @@ package edu.bu.met.cs665.database.repository;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
 
+import edu.bu.met.cs665.airplane.AirbusA320;
+import edu.bu.met.cs665.airplane.AirbusA380;
 import edu.bu.met.cs665.airplane.Airplane;
+import edu.bu.met.cs665.airplane.Boeing737;
+import edu.bu.met.cs665.airplane.Boeing747;
+import edu.bu.met.cs665.airplane.Boeing777;
+import edu.bu.met.cs665.airplane.Bombardier;
+import edu.bu.met.cs665.airplane.Embraer;
 import edu.bu.met.cs665.database.Database;
+import edu.bu.met.cs665.exception.InvalidDataException;
 
 public class AirplaneRepository implements IRepository<Airplane> {
 
@@ -62,6 +71,50 @@ public class AirplaneRepository implements IRepository<Airplane> {
     @Override
     public List<Airplane> selectAll() throws SQLException {
         List<Airplane> airplanes = new ArrayList<>();
+        String sql = "SELECT * FROM airplanes";
+
+        try (Connection conn = Database.connect();
+                Statement stmt = conn.createStatement();
+                ResultSet rs = stmt.executeQuery(sql)) {
+
+            while (rs.next()) {
+                String name = rs.getString("name");
+                int range = rs.getInt("range");
+                int fuelCapacity = rs.getInt("fuel_capacity");
+                int fuelConsumption = rs.getInt("fuel_consumption");
+                int speed = rs.getInt("speed");
+                Airplane airplane = createAirplane(name, range, fuelCapacity, fuelConsumption, speed);
+                if (airplane != null) {
+                    airplanes.add(airplane);
+                }
+            }
+        } catch (SQLException e) {
+            System.out.println("Error selecting airplanes: " + e.getMessage());
+        } catch (InvalidDataException e) {
+            System.out.println("InvalidDataException selecting locations: " + e.getMessage());
+        }
         return airplanes;
+    }
+
+    private Airplane createAirplane(String name, double range, double fuelCapacity, double fuelConsumption,
+            double speed) throws InvalidDataException {
+        switch (name) {
+            case "Boeing 737":
+                return new Boeing737(name, range, fuelCapacity, fuelConsumption, speed);
+            case "Boeing 747":
+                return new Boeing747(name, range, fuelCapacity, fuelConsumption, speed);
+            case "Boeing 777":
+                return new Boeing777(name, range, fuelCapacity, fuelConsumption, speed);
+            case "Airbus A380":
+                return new AirbusA380(name, range, fuelCapacity, fuelConsumption, speed);
+            case "Airbus A320":
+                return new AirbusA320(name, range, fuelCapacity, fuelConsumption, speed);
+            case "Bombardier CRJ200":
+                return new Bombardier(name, range, fuelCapacity, fuelConsumption, speed);
+            case "Embraer E1900":
+                return new Embraer(name, range, fuelCapacity, fuelConsumption, speed);
+            default:
+                return null;
+        }
     }
 }
