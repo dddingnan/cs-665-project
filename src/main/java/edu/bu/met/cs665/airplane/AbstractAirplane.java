bd22901c9f33fc79498129cb93146db1fff0d712
dddingnan <silenceapple1027@gmail.com>
2023-11-28 14:51:49 -0500

    feat: add airplanes comment.

--- src/main/java/edu/bu/met/cs665/airplane/AbstractAirplane.java
+++ src/main/java/edu/bu/met/cs665/airplane/AbstractAirplane.java
@@ -1,3 +1,12 @@
+/**
+ * Name: Dingnan Hsu
+ * Course: CS-665 Software Designs & Patterns
+ * Date: 11/21/2023
+ * File Name: AbstractAirplane.java
+ * Description: The AbstractAirplane class serves as a base for 
+ * different airplane types in the flight
+ * simulation system.
+ */
 package edu.bu.met.cs665.airplane;
 
 import edu.bu.met.cs665.exception.InvalidDataException;
@@ -9,6 +18,19 @@ public abstract class AbstractAirplane implements Airplane {
     protected double fuelBurnRate; // in gallons per hour
     protected double speed; // added speed attribute
 
+    /**
+     * Initializes a new airplane instance with specified attributes. Validates the
+     * input
+     * data for fuel capacity and fuel burn rate to ensure they are within
+     * acceptable ranges.
+     * 
+     * @param name         The name of the airplane.
+     * @param range        The flying range of the airplane.
+     * @param fuelCapacity The fuel capacity of the airplane in gallons.
+     * @param fuelBurnRate The fuel burn rate of the airplane in gallons per hour.
+     * @param speed        The speed of the airplane.
+     * @throws InvalidDataException If fuel capacity or fuel burn rate is invalid.
+     */
     public AbstractAirplane(String name, double range, double fuelCapacity, double fuelBurnRate, double speed)
             throws InvalidDataException {
         if (fuelCapacity < 0 || fuelBurnRate < 0) {
@@ -22,26 +44,51 @@ public abstract class AbstractAirplane implements Airplane {
         this.speed = speed;
     }
 
+    /**
+     * Gets the name of the airplane.
+     * 
+     * @return The name of the airplane.
+     */
     @Override
     public String getName() {
         return this.name;
     }
 
+    /**
+     * Gets the flying range of the airplane.
+     * 
+     * @return The range of the airplane.
+     */
     @Override
     public double getRange() {
         return this.range;
     }
 
+    /**
+     * Gets the fuel capacity of the airplane.
+     * 
+     * @return The fuel capacity in gallons.
+     */
     @Override
     public double getFuelCapacity() {
         return this.fuelCapacity;
     }
 
+    /**
+     * Gets the fuel burn rate of the airplane.
+     * 
+     * @return The fuel burn rate in gallons per hour.
+     */
     @Override
     public double getFuelBurnRate() {
         return this.fuelBurnRate;
     }
 
+    /**
+     * Gets the speed of the airplane.
+     * 
+     * @return The speed of the airplane.
+     */
     @Override
     public double getSpeed() {
         return this.speed;
--- src/main/java/edu/bu/met/cs665/airplane/Airplane.java
+++ src/main/java/edu/bu/met/cs665/airplane/Airplane.java
@@ -1,6 +1,18 @@
+/**
+ * Name: Dingnan Hsu
+ * Course: CS-665 Software Designs & Patterns
+ * Date: 11/21/2023
+ * File Name: Airplane.java
+ * Description: Defines the interface for an airplane. 
+ * This interface includes methods to access
+ * various airplane characteristics such as name, 
+ * range, fuel capacity, burn rate, 
+ * consumption, and speed.
+ */
 package edu.bu.met.cs665.airplane;
 
 public interface Airplane {
+    // Interface methods
     String getName();
 
     double getRange();
